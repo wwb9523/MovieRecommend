b4516cec3087a014111f7de7e16dc7fe9e17104f
wwb9523 <48450976@qq.com>
2017-04-10 21:21:26 +0800

    add train and test

--- itemBasedCF.py
+++ itemBasedCF.py
@@ -21,6 +21,7 @@ class ItemBasedCF:
             user,item,score,_ = line.strip().split("\t")
             self.test.setdefault(user,{})
             self.test[user][item] = int(score)
+
     def ItemSimilarity(self):
         #建立物品-物品的共现矩阵
         C = dict()  #物品-物品的共现矩阵
@@ -41,6 +42,7 @@ class ItemBasedCF:
             for j,cij in related_items.items():
                 self.W[i][j] = cij / (math.sqrt(N[i] * N[j]))
         return self.W
+
     #给用户user推荐，前K个相关用户
     def Recommend(self,user,K=3,N=10):
         rank = dict()
@@ -55,3 +57,60 @@ class ItemBasedCF:
                 # rank[I] = rank[I] / sim_sum
         return dict(sorted(rank.items(),key=lambda x:x[1],reverse=True)[0:N])
 
+    # 召回率和准确率
+    def RecallAndPrecision(self, train=None, test=None, K=3, N=10):
+        train = train or self.train
+        test = test or self.test
+        hit = 0
+        recall = 0
+        precision = 0
+        for user in train.keys():
+            tu = test.get(user, {})
+            rank = self.Recommend(user, K=K, N=N)
+            for i, _ in rank.items():
+                if i in tu:
+                    hit += 1
+            recall += len(tu)
+            precision += N
+        recall = hit / (recall * 1.0)
+        precision = hit / (precision * 1.0)
+        return (recall, precision)
+
+    # 覆盖率
+    def Coverage(self, train=None, test=None, K=3, N=10):
+        train = train or self.train
+        recommend_items = set()
+        all_items = set()
+        for user, items in train.items():
+            for i in items.keys():
+                all_items.add(i)
+            rank = self.Recommend(user, K)
+            for i, _ in rank.items():
+                recommend_items.add(i)
+        return len(recommend_items) / (len(all_items) * 1.0)
+
+    # 新颖度
+    def Popularity(self, train=None, test=None, K=3, N=10):
+        train = train or self.train
+        item_popularity = dict()
+        # 计算物品流行度
+        for user, items in train.items():
+            for i in items.keys():
+                item_popularity.setdefault(i, 0)
+                item_popularity[i] += 1
+        ret = 0  # 新颖度结果
+        n = 0  # 推荐的总个数
+        for user in train.keys():
+            rank = self.Recommend(user, K=K, N=N)  # 获得推荐结果
+            for item, _ in rank.items():
+                ret += math.log(1 + item_popularity[item])
+                n += 1
+        ret /= n * 1.0
+        return ret
+
+if __name__=='__main__':
+    itemBasedCF=ItemBasedCF('train.data','test.data')
+    itemBasedCF.ItemSimilarity()
+    print(itemBasedCF.RecallAndPrecision())
+    print(itemBasedCF.Coverage())
+    print(itemBasedCF.Popularity())
