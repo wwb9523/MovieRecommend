7f3dd78daafb214c7ca9b474510da2f81a2405af
Davic Wei <48450976@qq.com>
2017-04-11 15:10:28 +0800

    修改聚类评估

--- recommend.py
+++ recommend.py
@@ -5,7 +5,7 @@ import multiprocessing
 from kmeans import KMeansClassifier
 import pickle
 import os,time
-from function import distMovie
+
 
 class Recommend(object):
     def __init__(self,k=3, initCent='random', max_iter=500):
@@ -40,7 +40,7 @@ class Recommend(object):
         print(clf._labels)
         print(clf._centroids)
         print(clf._sse)
-        pkl="clf"+str(limit)+"_"+str(k)+".pkl"
+        pkl="clf"+str(limit)+"_"+str(k)+'_'+str(clf._sse)+".pkl"
         if os.path.exists(pkl):
             input=open(pkl,'rb')
             mk=pickle.load(input)
@@ -69,75 +69,7 @@ def loop():
     p.close()
     p.join()
 
-def Silhouette(mk):
-    label = mk._labels
-    labels={}
-    for i in range(mk._k):
-        labels.setdefault(i+1,{})
-        value = np.nonzero(label == i + 1)[0]
-        labels[i+1]=value
-    siAll=0
-    for i in range(len(mk._labels)):
-        si=getSi(labels,i)
-        siAll=siAll+si
-    Sil=siAll/len(mk._labels)
-    print(Sil)
-    return Sil
 
-
-def getAi(labels,i):
-    label=[]
-    for key,item in labels.items():
-        if i in item:
-            label=item
-            break
-    if i not in label:
-        return -1
-    mydb = MyDB()
-    dsAll=0
-    for index in label:
-        distance = mydb.getSimById(i+1, index+1)
-        if not distance:
-            movie1 = Movie().getMovieById(i+1)
-            movie2 = Movie().getMovieById(index+1)
-            distance = distMovie(movie1, movie2)
-            mydb.insertDistance(i+1, index+1, distance)
-        dsAll=dsAll+distance
-    mydb.db.commit()
-    mydb.db.close()
-    ai=dsAll/len(label)
-    return ai
-
-
-def getBi(labels,i):
-    ds=[]
-    mydb = MyDB()
-    for key, item in labels.items():
-        if i in item:
-            continue
-        dsAll=0
-        for index in item:
-            distance = mydb.getSimById(i + 1, index + 1)
-            if not distance:
-                movie1 = Movie().getMovieById(i + 1)
-                movie2 = Movie().getMovieById(index + 1)
-                distance = distMovie(movie1, movie2)
-                mydb.insertDistance(i+1, index+1, distance)
-            dsAll = dsAll + distance
-        ds.append(dsAll)
-    mydb.db.commit()
-    mydb.db.close()
-    bi=min(ds)
-    return bi
-
-def getSi(labels,i):
-    ai=getAi(labels,i)
-    print('ai: %s'%ai)
-    bi=getBi(labels,i)
-    print('bi: %s'%bi)
-    si=(bi-ai)/max([ai,bi])
-    print('si: %s'%si)
-    return si
 
 if __name__=='__main__':
     #loop()
